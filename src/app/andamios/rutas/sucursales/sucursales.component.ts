4633c6fee4dfdc0bf64e8ecb122c485c3160a8b6
Fermin Jimenez Garcia <NotAUserName0>
2025-06-14 11:02:44 -0600

    imp(PROD): Solucionados WIPS en area Andamios

--- src/app/andamios/rutas/sucursales/sucursales.component.ts
+++ src/app/andamios/rutas/sucursales/sucursales.component.ts
@@ -1,48 +1,59 @@
-import { afterRender, Component } from '@angular/core';
+import { afterRender, Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { fadeInAnimation } from '../../../fadeIn';
 import { AndamiosService } from '../../andamios.service';
 import { PetitionsService } from '../../../petitions.service';
 import { Sucursal } from '../../models/sucursal';
-import { after } from 'node:test';
 import { Imagen } from '../../models/imagen';
+import Swal from 'sweetalert2';
+import { SpinnerComponent } from '../../../spinner/spinner.component';
+
+const IMG_NAME = 'andamios';
 
 @Component({
   selector: 'app-sucursales',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, SpinnerComponent],
   templateUrl: './sucursales.component.html',
   styleUrl: './sucursales.component.css',
   animations: [fadeInAnimation]
 })
-export class SucursalesComponent {
-  sucursales: Sucursal[]
-  img: any
-  loading: boolean = false;
+export class SucursalesComponent implements OnInit {
+  public sucursales: Sucursal[]
+  public img: any
+  public loading: boolean = false;
 
   constructor(private andamiosService: AndamiosService, private petitionsService: PetitionsService) {
-
-    this.loading = true;
-    this.obtenerImagen();
-    this.obtenerSucursales()
-
     afterRender(() => {
       window.scroll(0, 0);
-    })
-
-    this.loading = false;
+    });
   }
 
-  obtenerImagen() {
-    this.andamiosService.obtenerImaganSucursal('andamios').subscribe((data: Imagen) => {
-      //data.file = this.petitionsService.sanitizar(data.file);
+  public ngOnInit() {
+    this.loading = true;
+    this.obtenerImagen();
+    this.loading = true;
+    this.obtenerSucursales();
+  }
+
+  public obtenerImagen(): void {
+    this.andamiosService.obtenerImaganSucursal(IMG_NAME).subscribe((data: Imagen) => {
       this.img = data;
-    }
-    )
+      this.loading = false;
+    }, (error) => {
+      Swal.fire({
+        title: 'Error',
+        text: 'No se pudo cargar la imagen de la sucursal',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      }).then(() => {
+        this.loading = false;
+      });
+    })
   }
 
-    obtenerSucursales() {
-    this.andamiosService.obtenerSucursales('andamios').subscribe((data: Sucursal[]) => {
+  public obtenerSucursales(): void {
+    this.andamiosService.obtenerSucursales(IMG_NAME).subscribe((data: Sucursal[]) => {
       data.forEach(element => {
         // Asegurarse de que telefono sea un string
         const telefonoStr = String(element.telefono); // Convertir a string si no lo es
@@ -51,6 +62,16 @@ export class SucursalesComponent {
         element.telefono = arrTelefonos; // Asignar el array si el campo telefono es any
       });
       this.sucursales = data;
-    })
+      this.loading = false;
+    }, (error) => {
+      Swal.fire({
+        title: 'Error',
+        text: 'No se pudo cargar las sucursales',
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      }).then(() => {
+        this.loading = false;
+      });
+    });
   }
 }
